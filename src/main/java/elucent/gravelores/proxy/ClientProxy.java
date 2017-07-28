4c78263ca1777d5e55d69436cb026f2dafb43a36
Sunconure11 <eyespot7b@aol.com>
2017-07-28 12:39:18 -0400

    Revert "Ran through reformatter"
    This reverts commit 8d06537c7d67e09d999ecf969e9e7970c73624a6.

--- src/main/java/elucent/gravelores/proxy/ClientProxy.java
+++ src/main/java/elucent/gravelores/proxy/ClientProxy.java
@@ -1,10 +1,11 @@
 package elucent.gravelores.proxy;
 
+import elucent.gravelores.GravelOres;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
 	@Override
-	public void preInit(FMLPreInitializationEvent event) {
+	public void preInit(FMLPreInitializationEvent event){
 		super.preInit(event);
 	}
 }
--- src/main/java/elucent/gravelores/world/WorldGenGravelOres.java
+++ src/main/java/elucent/gravelores/world/WorldGenGravelOres.java
@@ -1,5 +1,9 @@
 package elucent.gravelores.world;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import elucent.gravelores.ConfigManager;
 import elucent.gravelores.GravelOres;
 import net.minecraft.block.Block;
@@ -13,37 +17,33 @@ import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class WorldGenGravelOres implements IWorldGenerator {
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-	                     IChunkProvider chunkProvider) {
-		if (world.provider.getDimensionType() == DimensionType.OVERWORLD) {
-			if (random.nextInt(ConfigManager.orePileChance) == 0) {
+			IChunkProvider chunkProvider) {
+		if (world.provider.getDimensionType() == DimensionType.OVERWORLD){
+			if (random.nextInt(ConfigManager.orePileChance) == 0){
 				Block b = GravelOres.spawns.get(random.nextInt(GravelOres.spawns.size()));
-				if (b != null) {
+				if (b != null){
 					IBlockState ore = b.getDefaultState();
-					int xx = chunkX * 16 + 8 + random.nextInt(16);
-					int zz = chunkZ * 16 + 8 + random.nextInt(16);
-					BlockPos top = world.getTopSolidOrLiquidBlock(new BlockPos(xx, 64, zz));
+					int xx = chunkX*16 + 8 + random.nextInt(16);
+					int zz = chunkZ*16 + 8 + random.nextInt(16);
+					BlockPos top = world.getTopSolidOrLiquidBlock(new BlockPos(xx,64,zz));
 					IBlockState state = world.getBlockState(top);
-					if (state.getBlock() != Blocks.WATER && state.isOpaqueCube()) {
-						if (state.getBlock() == Blocks.AIR || state.getBlock().isReplaceable(world, top)) {
-							int tries = ConfigManager.orePileMinSize + random.nextInt(ConfigManager.orePileMaxSize - ConfigManager.orePileMinSize);
+					if (state.getBlock() != Blocks.WATER && state.isOpaqueCube()){
+						if (state.getBlock() == Blocks.AIR || state.getBlock().isReplaceable(world, top)){
+							int tries = ConfigManager.orePileMinSize+random.nextInt(ConfigManager.orePileMaxSize-ConfigManager.orePileMinSize);
 							List<BlockPos> blocks = new ArrayList<BlockPos>();
 							blocks.add(top);
-							for (int i = 0; i < tries; i++) {
+							for (int i = 0; i < tries; i ++){
 								BlockPos pos = blocks.get(random.nextInt(blocks.size()));
 								EnumFacing face = EnumFacing.getFront(random.nextInt(6));
 								IBlockState state2 = world.getBlockState(pos.offset(face));
-								if (state2.getBlock() == Blocks.AIR || state2.getBlock().isReplaceable(world, pos.offset(face))) {
+								if (state2.getBlock() == Blocks.AIR || state2.getBlock().isReplaceable(world, pos.offset(face))){
 									blocks.add(pos.offset(face));
 								}
 							}
-							for (int i = 0; i < blocks.size(); i++) {
+							for (int i = 0; i < blocks.size(); i ++){
 								world.setBlockState(blocks.get(i), ore);
 							}
 						}
