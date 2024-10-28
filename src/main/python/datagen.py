ddc68ceba3db55fec38c1d1993dada8cdfe3cfb2
KnightMiner <knightminer4@gmail.com>
2024-10-28 00:17:04 -0400

    Implement tag generator for mineable tags

--- src/main/python/datagen.py
+++ src/main/python/datagen.py
@@ -6,6 +6,7 @@ from gravelores.logger import setupLogging
 from gravelores.blocks import BlockGenerator
 from gravelores.loot import LootTableGenerator
 from gravelores.models import ModelGenerator
+from gravelores.tags import TagGenerator
 
 
 ROOT_PATH = "../../generated/resources"
@@ -27,20 +28,25 @@ ORES = {
         "drop": {
             "drop": "minecraft:raw_copper",
             "count": (2, 5)
-        }
+        },
+        "tier": "stone"
     },
     "iron": {
-        "drop": "minecraft:raw_iron"
+        "drop": "minecraft:raw_iron",
+        "tier": "stone"
     },
     "gold": {
-        "drop": "minecraft:raw_gold"
+        "drop": "minecraft:raw_gold",
+        "tier": "iron"
     },
     # gems
     "diamond": {
-        "drop": "minecraft:diamond"
+        "drop": "minecraft:diamond",
+        "tier": "iron"
     },
     "emerald": {
-        "drop": "minecraft:emerald"
+        "drop": "minecraft:emerald",
+        "tier": "iron"
     },
     "quartz": {
         "drop": "minecraft:quartz"
@@ -54,13 +60,15 @@ ORES = {
             "drop": "minecraft:redstone",
             "count": (4, 5),
             "flatBonus": True
-        }
+        },
+        "tier": "iron"
     },
     "lapis": {
         "drop": {
             "drop": "minecraft:lapis_lazuli",
             "count": (4, 9)
-        }
+        },
+        "tier": "stone"
     }
 }
 """General config for all datagen to run, puts it in one nice spot"""
@@ -75,6 +83,7 @@ if __name__ == "__main__":
     # saves writing `_gravel_ore` everywhere
     for variant, data in ORES.items():
         data["name"] = variant + "_gravel_ore" 
+        data["id"] = MOD_ID + ":" + data["name"]
     
     with ModelGenerator(cache) as gen:
         for data in ORES.values():
@@ -91,6 +100,12 @@ if __name__ == "__main__":
                 gen.oreBlock(MOD_ID, data["name"], **drop)
             else:
                 gen.oreBlock(MOD_ID, data["name"], drop=drop)
+    
+    with TagGenerator(cache) as gen:
+        gen.add("blocks", "minecraft", "mineable/shovel", *[data["id"] for data in ORES.values()])
+        for data in ORES.values():
+            if "tier" in data:
+                gen.add("blocks", "minecraft", f"needs_{data['tier']}_tool", data["id"])
     
     # end of datagen, save the cache file
     cache.finalize()
---
+++ src/main/python/gravelores/tags.py
@@ -0,0 +1,58 @@
+import logging
+from time import perf_counter
+from typing import Any, Dict, List, Union, Tuple
+from .cache import CachedOutput
+
+DATA_ROOT = "data"
+TAG_ROOT = "tags"
+
+class TagGenerator:
+    """
+    Handles loot table generation.
+    Doing mostly simple tables, anything complex we just do manually.
+    """
+    
+    cache: CachedOutput
+    """Cache instance for saving files"""
+    tags: Dict[Tuple[str, str], List[Union[str, Dict[str, Any]]]]
+    
+    def __init__(self, cache: CachedOutput):
+        self.cache = cache
+        self.tags = {}
+        self.time = 0
+    
+    def __enter__(self) -> "ModelGenerator":
+        logging.info("Starting loot table generation")
+        self.time = perf_counter()
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        for (domain, name), values in self.tags.items():
+            # registry name is baked into the tag name
+            self.cache.saveJson({"values": values}, DATA_ROOT, domain, "tags", name)
+        
+        self.time = perf_counter() - self.time
+        logging.info(f"Generated {len(self.tags)} tags in {self.time} seconds")
+        self.tags = {}
+        return False
+    
+    def _enforceTagExists(self, registry: str, domain: str, name: str) -> Tuple[str, str]:
+        # combine registry and tag name
+        key = (domain, f"{registry}/{name}")
+        if not key in self.tags:
+            self.tags[key] = []
+        return key
+    
+    
+    # Methods for datagen root to call
+
+    def add(self, registry: str, domain: str, name: str, *values: str) -> None:
+        """Appends the given values to the given tag. Prefix a name with a # to append a tag."""
+        key = self._enforceTagExists(registry, domain, name)
+        self.tags[key].extend(values)
+    
+    def addOptional(self, registry: str, domain: str, name: str, *values: str) -> None:
+        """Appends the given values as optional tag entries."""
+        key = self._enforceTagExists(registry, domain, name)
+        self.tags[key].extend([{"id": value, "required": False} for value in values])
+        
