b5238d3f5904a83910d921cced2f8b912ffaf10e
KnightMiner <knightminer4@gmail.com>
2024-10-29 15:53:16 -0400

    Add support for compat from various mods
    Tinkers' Construct: Cobalt
    Create: Zinc
    Thermal: Lead, Nickel, Silver, Tin
    Immersive Engineering: Aluminum, Lead, Nickel, Silver, Uranium
    Embers: Lead, Silver
    Mekansism: Lead, Osmium, Tin, Uranium
    
    If the relevant mod(s) are missing for a given ore type, the block is still registered but the worldgen is skipped.
    Due to limitations in the loot table syntax, if multiple supported mods exist, the ore will randomly drop the raw ore from one of those mods. This can be configured to drop a specific raw ore by replacing the item tag `gravelores:ore_drops/<variant>`

--- src/main/python/datagen.py
+++ src/main/python/datagen.py
@@ -81,6 +81,62 @@ ORES = {
         },
         "tier": "stone",
         "overworld_chance": 1000  # 2 + 4 buried
+    },
+    
+    # compat
+    "aluminum": {
+        "mods": ["immersiveengineering"],
+        "tier": "stone",
+        "tag_drop": "raw_materials",
+        "overworld_chance": 750  # Immersive Engineering uses weird ore config so guessing
+    },
+    "cobalt": {
+        "mods": ["tconstruct"],
+        "tier": "iron",
+        "tag_drop": "raw_materials",
+        "nether_chance": 300  # Rarer than gold
+    },
+    "lead": {
+        "mods": ["embers", "immersiveengineering", "thermal", "mekanism"],
+        "tier": "iron",  # mekansim says stone, consensus iron
+        "tag_drop": "raw_materials",
+        "overworld_chance": 750  # Embers 8, Thermal 6, Mek 8
+    },
+    "nickel": {
+        "mods": ["immersiveengineering", "thermal"],
+        "tier": "iron",
+        "tag_drop": "raw_materials",
+        "overworld_chance": 850  # Thermal 4
+    },
+    "osmium": {
+        "mods": ["mekanism"],
+        "tier": "stone",
+        "tag_drop": "raw_materials",
+        "overworld_chance": 450  # Mek: 65 + 6 + 8
+    },
+    "silver": {
+        "mods": ["embers", "immersiveengineering", "thermal"],
+        "tier": "iron",
+        "tag_drop": "raw_materials",
+        "overworld_chance": 850  # Embers 4, Thermal 4
+    },
+    "tin": {
+        "mods": ["thermal", "mekanism"],
+        "tier": "stone",
+        "tag_drop": "raw_materials",
+        "overworld_chance": 650  # Mek: 14 + 12, Thermal 6
+    },
+    "uranium": {
+        "mods": ["immersiveengineering", "mekanism"],
+        "tier": "iron",  # mekanism says stone, but it already violated concensus
+        "tag_drop": "raw_materials",
+        "overworld_chance": 1000  # Mek: 4 + 7 (buried)
+    },
+    "zinc": {
+        "mods": ["create"],
+        "tier": "iron",
+        "tag_drop": "raw_materials",
+        "overworld_chance": 750  # Create: 8
     }
 }
 """General config for all datagen to run, puts it in one nice spot"""
@@ -98,6 +154,13 @@ if __name__ == "__main__":
         data["pile"] = data["name"] + "_pile"
         data["id"] = MOD_ID + ":" + data["name"]
         data["pile_id"] = MOD_ID + ":" + data["pile"]
+        
+        # tag_drop is used later to define which tag, here it just generates drop data
+        if "tag_drop" in data:
+            if not "drop" in data:
+                data["drop"] = {}
+            data["drop"]["drop"] = f"gravelores:ore_drops/{variant}"
+            data["drop"]["tag"] = True
     
     with ModelGenerator(cache) as gen:
         for data in ORES.values():
@@ -118,20 +181,31 @@ if __name__ == "__main__":
     with WorldgenGenerator(cache) as gen:
         for data in ORES.values():
             if "overworld_chance" in data:
-                gen.addPile(MOD_ID, data["pile"], data["id"], data["overworld_chance"])
+                gen.addPile(MOD_ID, data["pile"], data["id"], data["overworld_chance"], data.get("mods"))
             if "nether_chance" in data:
-                gen.addPileNether(MOD_ID, data["pile"], data["id"], data["nether_chance"])
+                gen.addPileNether(MOD_ID, data["pile"], data["id"], data["nether_chance"], data.get("mods"))
     
     with TagGenerator(cache) as gen:
         gen.add("blocks", "minecraft", "mineable/shovel", *[data["id"] for data in ORES.values()])
-        for data in ORES.values():
+        for variant, data in ORES.items():
+            # harvest tiers
             if "tier" in data:
                 gen.add("blocks", "minecraft", f"needs_{data['tier']}_tool", data["id"])
+                
+            # tagging piles in the overworld tag adds them to overworld biomes
             if "overworld_chance" in data and not "custom_biomes" in data:
-                gen.add("worldgen/placed_feature", "gravelores", "overworld_piles", data["pile_id"])
+                gen.add("worldgen/placed_feature", "gravelores", "overworld_piles", data["pile_id"], optional = "mods" in data)
+            # tagging piles in the nether tag adds them to nether biomes
             if "nether_chance" in data:
                 # nether piles automatically append "_nether", so manually append for the tag
-                gen.add("worldgen/placed_feature", "gravelores", "nether_piles", data["pile_id"] + "_nether")
+                gen.add("worldgen/placed_feature", "gravelores", "nether_piles", data["pile_id"] + "_nether", optional = "mods" in data)
+            
+            # redirect gravelores:ore_drops/<variant> to forge:raw_materials/<variant>
+            # lets a modpack maker easily choose the drop for an ore without emptying the
+            # forge tag or redefining our loot table
+            if "tag_drop" in data:
+                gen.add("items", MOD_ID, f"ore_drops/{variant}", f"#forge:{data['tag_drop']}/{variant}", optional = True)
+                
     
     # end of datagen, save the cache file
     cache.finalize()
